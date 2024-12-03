4ac75a3a5e19c2c66b018486ab91fdba6bc936b7
K先森 <zhangyu528@users.noreply.github.com>
2024-12-03 05:44:12 +0000

    add unit test

--- Sources/ChainNetworkKit/ChainNetworkKit.swift
+++ Sources/ChainNetworkKit/ChainNetworkKit.swift
@@ -4,134 +4,84 @@
 import Foundation
 import FoundationNetworking
 
-public enum HTTPMethod: String {
+import Foundation
+
+/// HTTP Methods
+enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
     case put = "PUT"
     case delete = "DELETE"
 }
 
-public struct NetworkResponse {
-    public let data: Data?
-    public let statusCode: Int
-    public let headers: [AnyHashable: Any]
-
-    public func decode<T: Decodable>(to type: T.Type) throws -> T {
-        guard let data = data else {
-            throw NSError(domain: "ChainNetworkKit", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data available"])
-        }
-        return try JSONDecoder().decode(T.self, from: data)
-    }
+/// Network Request Error
+enum NetworkError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case decodingFailed
 }
 
-
-public class NetworkRequestBuilder {
+/// Network Request Builder with Chainable API
+final class NetworkRequestBuilder {
     private var url: URL?
     private var method: HTTPMethod = .get
     private var headers: [String: String] = [:]
-    private var parameters: [String: Any] = [:]
-    private var jsonRequest: Bool = true
+    private var body: Data?
 
-    public init() {}
-
-    // 设置请求 URL
-    @discardableResult
-    public func setURL(_ url: String) -> Self {
-        self.url = URL(string: url)
+    /// Set the URL
+    func setURL(_ urlString: String) -> NetworkRequestBuilder {
+        self.url = URL(string: urlString)
         return self
     }
 
-    // 设置 HTTP 方法
-    @discardableResult
-    public func setMethod(_ method: HTTPMethod) -> Self {
+    /// Set the HTTP Method
+    func setMethod(_ method: HTTPMethod) -> NetworkRequestBuilder {
         self.method = method
         return self
     }
 
-    // 添加请求头
-    @discardableResult
-    public func addHeader(key: String, value: String) -> Self {
+    /// Add a Header
+    func addHeader(key: String, value: String) -> NetworkRequestBuilder {
         self.headers[key] = value
         return self
     }
 
-    // 设置多个请求头
-    @discardableResult
-    public func setHeaders(_ headers: [String: String]) -> Self {
-        self.headers.merge(headers) { _, new in new }
+    /// Set the Request Body
+    func setBody<T: Encodable>(_ body: T) -> NetworkRequestBuilder {
+        self.body = try? JSONEncoder().encode(body)
         return self
     }
 
-    // 设置请求参数
-    @discardableResult
-    public func setParameters(_ parameters: [String: Any]) -> Self {
-        self.parameters = parameters
-        return self
-    }
-
-    // 设置是否为 JSON 请求
-    @discardableResult
-    public func setJSONRequest(_ jsonRequest: Bool) -> Self {
-        self.jsonRequest = jsonRequest
-        return self
-    }
-
-    // 发送请求
-    public func send(completion: @escaping @Sendable (Result<NetworkResponse, Error>) -> Void) {
+    /// Perform the request with completion handler
+    func execute<T: Decodable>(decodeTo type: T.Type, completion: @escaping @Sendable (Result<T, NetworkError>) -> Void) {
         guard let url = url else {
-            completion(.failure(NSError(domain: "ChainNetworkKit", code: -1, userInfo: [NSLocalizedDescriptionKey: "URL is missing"])))
+            completion(.failure(.invalidURL))
             return
         }
 
-        let request = createRequest(url: url)
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(NSError(domain: "ChainNetworkKit", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])))
-                return
-            }
-
-            let networkResponse = NetworkResponse(
-                data: data,
-                statusCode: httpResponse.statusCode,
-                headers: httpResponse.allHeaderFields
-            )
-            completion(.success(networkResponse))
-        }
-        task.resume()
-    }
-
-    private func createRequest(url: URL) -> URLRequest {
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+        request.httpBody = body
 
-        // 设置请求头
-        headers.forEach { request.setValue($1, forHTTPHeaderField: $0) }
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestFailed(error)))
+                return
+            }
 
-        // 设置请求参数
-        if let parameters = try? serializeParameters() {
-            if jsonRequest {
-                request.httpBody = parameters
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            } else if method != .get {
-                request.httpBody = parameters
-                request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            guard let data = data else {
+                completion(.failure(.decodingFailed))
+                return
+            }
+
+            do {
+                let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedResponse))
+            } catch {
+                completion(.failure(.decodingFailed))
             }
         }
-        return request
-    }
-
-    private func serializeParameters() throws -> Data? {
-        if jsonRequest {
-            return try JSONSerialization.data(withJSONObject: parameters, options: [])
-        } else {
-            let query = parameters.map { "\($0.key)=\($0.value)" }.joined(separator: "&")
-            return query.data(using: .utf8)
-        }
+        task.resume()
     }
 }
--- Tests/ChainNetworkKitTests/ChainNetworkKitTests.swift
+++ Tests/ChainNetworkKitTests/ChainNetworkKitTests.swift
@@ -1,7 +1,85 @@
 import Testing
-@testable import ChainNetworkKit
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     print("Tests")
+}
+
+import XCTest
+@testable import ChainNetworkKit
+
+/// Mock Response
+struct User: Codable {
+    let id: Int?
+    let name: String
+    let email: String
+}
+
+final class NetworkRequestBuilderTests: XCTestCase {
+    @MainActor
+    func testValidGetRequest() {
+        let expectation = self.expectation(description: "Valid GET request should succeed")
+
+        NetworkRequestBuilder()
+            .setURL("https://jsonplaceholder.typicode.com/users/1")
+            .setMethod(.get)
+            .execute(decodeTo: User.self) { result in
+                switch result {
+                case .success(let user):
+                    XCTAssertEqual(user.name, "Leanne Graham")
+                case .failure(let error):
+                    XCTFail("Request failed with error: \(error)")
+                }
+                
+                DispatchQueue.main.async {
+                    expectation.fulfill() // 确保在主线程上执行
+                }
+            }
+
+        waitForExpectations(timeout: 5.0)
+    }
+
+    // func testInvalidURL() {
+    //     let expectation = self.expectation(description: "Invalid URL should fail")
+
+    //     NetworkRequestBuilder()
+    //         .setURL("invalid-url")
+    //         .setMethod(.get)
+    //         .execute(decodeTo: User.self) { result in
+    //             switch result {
+    //             case .failure(let error):
+    //                 if case .invalidURL = error {
+    //                     expectation.fulfill()
+    //                 } else {
+    //                     XCTFail("Unexpected error: \(error)")
+    //                 }
+    //             default:
+    //                 XCTFail("Request should fail")
+    //             }
+    //         }
+
+    //     waitForExpectations(timeout: 5.0)
+    // }
+
+    // func testDecodingFailure() {
+    //     let expectation = self.expectation(description: "Decoding failure should occur")
+
+    //     NetworkRequestBuilder()
+    //         .setURL("https://jsonplaceholder.typicode.com/posts/1")
+    //         .setMethod(.get)
+    //         .execute(decodeTo: User.self) { result in
+    //             switch result {
+    //             case .failure(let error):
+    //                 if case .decodingFailed = error {
+    //                     expectation.fulfill()
+    //                 } else {
+    //                     XCTFail("Unexpected error: \(error)")
+    //                 }
+    //             default:
+    //                 XCTFail("Decoding should fail")
+    //             }
+    //         }
+
+    //     waitForExpectations(timeout: 5.0)
+    // }
 }
